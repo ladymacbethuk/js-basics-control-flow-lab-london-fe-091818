d3b545607f5ae82645f783063c0c31a4eacdb0bc
Stephanie Phillips <steph@ladymacbeth.uk>
2018-10-20 16:04:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+    if (distance > 2000 && distance < 2500) {
+      return 'I will gladly take your thirty bucks.';
+  }
+    if (distance > 2500) {
+      return 'No can do.';
+  }
+}
+
+/*
+const ternaryCheckCity;
+
+switch (ternaryCheckCity) {
+  case "NYC":
+    console.log = 'Ok, sounds good.';
+    break;
+  default:
+    console.log = 'No go.';
+    break;
+}*/
+
+//ternaryCheckCity === "NYC" ? 'Ok, sounds good.' : 'No go.';
+
+//const switchOnCharmFromTip = 'generous';
+
+
+function switchOnCharmFromTip(tip) {
+switch (tip) {
+  case 'generous':
+    console.log = 'Thank you so much.';
+    break;
+  case 'not generous':
+    console.log = 'Thank you.';
+    break;
+  default:
+    console.log =  'Bye.';
+  }
+}
